2f281a092bb12ec10613e53557ad9b3065276885
whboy <whboy_@outlook.com>
2025-02-14 21:59:07 +0800

    完结

--- src/router/index.js
+++ src/router/index.js
@@ -17,13 +17,6 @@ export const constantRoutes = [
     component: () => import('@/views/login/index'),
     hidden: true
   },
-
-  {
-    path: '/404',
-    component: () => import('@/views/404'),
-    hidden: true
-  },
-
   {
     path: '/',
     component: Layout,
@@ -34,56 +27,7 @@ export const constantRoutes = [
       component: () => import('@/views/dashboard/index'),
       meta: {title: 'Dashboard', icon: 'dashboard'}
     }]
-  },
-  {
-    path: '/system',
-    component: Layout,
-    meta: {
-      title: '系统管理',
-      icon: 'el-icon-s-tools'
-    },
-    alwaysShow: true,
-    children: [
-      {
-        path: 'sysRole',
-        component: () => import('@/views/system/sysRole/list'),
-        meta: {
-          title: '角色管理',
-          icon: 'el-icon-user-solid'
-        }
-      },
-      {
-        name: 'sysUser',
-        path: 'sysUser',
-        component: () => import('@/views/system/sysUser/list'),
-        meta: {
-          title: '用户管理',
-          icon: 'el-icon-s-custom'
-        },
-      },
-      {
-        name: 'sysMenu',
-        path: 'sysMenu',
-        component: () => import('@/views/system/sysMenu/list'),
-        meta: {
-          title: '菜单管理',
-          icon: 'el-icon-s-unfold'
-        },
-      },
-      {
-        path: 'assignAuth',
-        component: () => import('@/views/system/sysRole/assignAuth'),
-        meta: {
-          activeMenu: '/system/sysRole',
-          title: '角色授权'
-        },
-        hidden: true,
-      }
-    ]
-  },
-
-  // 404 page must be placed at the end !!!
-  {path: '*', redirect: '/404', hidden: true}
+  }
 ]
 
 const createRouter = () => new Router({
@@ -93,8 +37,6 @@ const createRouter = () => new Router({
 })
 
 const router = createRouter()
-
-// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
   const newRouter = createRouter()
   router.matcher = newRouter.matcher // reset router
--- src/utils/btn-permission.js
+++ src/utils/btn-permission.js
@@ -1,9 +1,5 @@
 import store from '@/store'
 
-/**
- * 判断当前用户是否有此按钮权限
- * @param {按钮权限字符串} permission
- */
 export default function hasBtnPermission(permission) {
   // 得到当前用户的所有按钮权限
   const myBtns = store.getters.buttons
