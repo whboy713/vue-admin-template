b7cfe3665431f7aa6964dfb3ec44a826c06553ef
whboy <whboy_@outlook.com>
2025-02-16 00:37:08 +0800

    Update vue.config.js
    生产环境

--- vue.config.js
+++ vue.config.js
@@ -37,11 +37,11 @@ module.exports = {
     },
     // before: require('./mock/mock-server.js')
     proxy: {
-      '/dev-api': { // 匹配所有以 '/dev-api'开头的请求路径
+      '/prod-api': { // 匹配所有以 '/dev-api'开头的请求路径
         target: 'http://localhost:8800',
         changeOrigin: true, // 支持跨域
         pathRewrite: { // 重写路径: 去掉路径中开头的'/dev-api'
-          '^/dev-api': ''
+          '^/prod-api': ''
         }
       }
     }
